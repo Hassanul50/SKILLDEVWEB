5f06b22606ed40e8b44d7efabd02a91debf5c394
hasan50 <hasanakib27@gmail.com>
2024-08-17 02:54:53 +0600

    Product model Added

--- SKILLDEVWEB.DataAccess/Data/ApplicationDbContext.cs
+++ SKILLDEVWEB.DataAccess/Data/ApplicationDbContext.cs
@@ -10,6 +10,7 @@ namespace SKILLDEVWEB.DataAccess.Data
 
         }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Category>().HasData(
@@ -17,6 +18,45 @@ namespace SKILLDEVWEB.DataAccess.Data
                 new Category { CategoryId = 2, CategoryName = "Scifi", DisplayOrder = 2 },
                 new Category { CategoryId = 3, CategoryName = "History", DisplayOrder = 3 },
                 new Category { CategoryId = 4, CategoryName = "Cartoon", DisplayOrder = 4 }
+                );
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    ProductId = 1,
+                    Title = "ABC",
+                    Author = "ABS",
+                    Description = "TEST",
+                    ISBN = "ABC-123",
+                    ListPrice = 30,
+                    Price = 25,
+                    Price50 = 20,
+                    Price100 = 18
+                },
+                new Product
+                {
+                    ProductId = 2,
+                    Title = "TEST-2",
+                    Author = "ABS-2",
+                    Description = "TEST-2",
+                    ISBN = "ABC-645",
+                    ListPrice = 54,
+                    Price = 47,
+                    Price50 = 32,
+                    Price100 = 20
+                },
+                new Product
+                {
+                    ProductId = 3,
+                    Title = "ABC-3",
+                    Author = "ABS-3",
+                    Description = "TEST-3",
+                    ISBN = "ABC-1485",
+                    ListPrice = 301,
+                    Price = 252,
+                    Price50 = 208,
+                    Price100 = 180
+                }
+
                 );
         }
     }
---
+++ SKILLDEVWEB.DataAccess/Migrations/20240816204431_AddedProductModel.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace SKILLDEVWEB.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedProductModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ISBN = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ListPrice = table.Column<double>(type: "float", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    Price50 = table.Column<double>(type: "float", nullable: false),
+                    Price100 = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "Author", "Description", "ISBN", "ListPrice", "Price", "Price100", "Price50", "Title" },
+                values: new object[,]
+                {
+                    { 1, "ABS", "TEST", "ABC-123", 30.0, 25.0, 18.0, 20.0, "ABC" },
+                    { 2, "ABS-2", "TEST-2", "ABC-645", 54.0, 47.0, 20.0, 32.0, "TEST-2" },
+                    { 3, "ABS-3", "TEST-3", "ABC-1485", 301.0, 252.0, 180.0, 208.0, "ABC-3" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
